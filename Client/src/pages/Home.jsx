2d6a061725e67f4eaadc0c33286fde6924a80e27
salgxv <salvarela09@yahoo.com>
2025-04-21 17:03:37 -0700

    fixed

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import GpuCard from '../components/Gpucard'
+import GpuCard from '../components/GpuCard.jsx'
 
 
 
